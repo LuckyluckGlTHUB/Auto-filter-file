31d0bee7af0888b5037a777de2162a1b78682154
Luckyluck <122548151+LuckyluckGlTHUB@users.noreply.github.com>
2023-11-28 10:57:51 +0100

    Update Main.py (english)

--- Main.py
+++ Main.py
@@ -20,48 +20,48 @@ for folder in folders:
         ftp.connect(config['server'])
         ftp.login(config['id'], config['passwd'])
         ftp.set_pasv(0)
-        print(f"Connecté au FTP de {config['server']} - Dossier : {folder}")
+        print(f"Connected to FTP server {config['server']} - Folder: {folder}")
 
         ftp.cwd(config['path_ftp'])
 
-        liste_fichiers = [fichier[0] for fichier in ftp.mlsd() if fichier[1]['type'] == 'file']
+        file_list = [file[0] for file in ftp.mlsd() if file[1]['type'] == 'file']
 
-        fichiers_avec_taille = []
+        files_with_size = []
 
-        for fichier in liste_fichiers:
+        for file in file_list:
             try:
-                taille_fichier = ftp.size(fichier)
+                file_size = ftp.size(file)
 
-                if taille_fichier == 0:
-                    print(f"Ignorer le fichier {fichier} (0 octet)")
+                if file_size == 0:
+                    print(f"Ignore file {file} (0 bytes)")
                     continue
 
-                if not fichier.endswith(config['extension']):
-                    print(f"Ignorer le fichier {fichier} (extension non valide)")
+                if not file.endswith(config['extension']):
+                    print(f"Ignore file {file} (invalid extension)")
                     continue
 
-                fichiers_avec_taille.append((fichier, taille_fichier))
+                files_with_size.append((file, file_size))
 
             except Exception as e:
-                print(f"Erreur lors de la récupération de la taille du fichier {fichier}: {e}")
+                print(f"Error getting file size for {file}: {e}")
 
-        if fichiers_avec_taille:
-            fichiers_avec_taille.sort(key=lambda x: x[1], reverse=False)
-            fichier_a_garder = fichiers_avec_taille[0][0]
-            print(f"Conserver le fichier avec la plus grande taille : {fichier_a_garder}")
+        if files_with_size:
+            files_with_size.sort(key=lambda x: x[1], reverse=False)
+            file_to_keep = files_with_size[0][0]
+            print(f"Keep file with the largest size: {file_to_keep}")
 
-            fichiers_txt_non_nuls = [fichier[0] for fichier in fichiers_avec_taille if fichier[1] > 0]
-            if len(fichiers_txt_non_nuls) > 1:
-                for fichier in liste_fichiers:
-                    if fichier == fichier_a_garder:
-                        ftp.delete(fichier)
-                        print(f"Le fichier {fichier} a été supprimé")
+            non_empty_txt_files = [file[0] for file in files_with_size if file[1] > 0]
+            if len(non_empty_txt_files) > 1:
+                for file in file_list:
+                    if file == file_to_keep:
+                        ftp.delete(file)
+                        print(f"The file {file} has been deleted")
             else:
-                print(f"Il ne reste qu'un seul fichier .txt (non à 0 octet) sur {config['serveur']} - Dossier : {folder}. Ne pas supprimer.")
+                print(f"There is only one non-empty .txt file on {config['server']} - Folder: {folder}. Do not delete.")
     except error_perm as e_perm:
-        print(f"Erreur de permission sur {config['server']} - Dossier : {folder}: {e_perm}")
+        print(f"Permission error on {config['server']} - Folder: {folder}: {e_perm}")
     except Exception as e:
-        print(f"Une erreur inattendue s'est produite sur {config['server']} - Dossier : {folder}: {e}")
+        print(f"An unexpected error occurred on {config['server']} - Folder: {folder}: {e}")
     finally:
-        print(f'Déconnexion au FTP de {config["server"]} - Dossier : {folder}')
+        print(f'Disconnected from FTP server {config["server"]} - Folder: {folder}')
         ftp.quit()
